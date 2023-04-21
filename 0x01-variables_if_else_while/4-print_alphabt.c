7c2970a1e535d0ee0d9d2b291dd10d37007aa9a3
irisd13 <myemail@example.com>
2023-04-21 06:08:06 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the value of n status:
+ *      greater than, is zero and is not less then 6
+ *
+ * Return: Always  0 (seccess)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (CH <= 'Z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
