b9d4d7f18da7fe59e20a87e819eac27b9388a81a
irisd13 <myemail@example.com>
2023-04-26 17:00:57 -0700

    a

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: comptes the sum of all
+ * the multiples of 3 or 5
+ * below 1023 (excluded)
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
