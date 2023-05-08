ee2acdf2216b2de018fc2f4fec7fd0fb4308eccb
irisd13 <myemail@example.com>
2023-05-08 14:14:16 -0700

    0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int itr;
 
-	for (; n > 0; i++)
+	for (itr =0; n > 0; itr++, n--)
 	{
-		s[i] = b;
-		n--;
+		s[itr] = b;
 	}
 	return (s);
 }
